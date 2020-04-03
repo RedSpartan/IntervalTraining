c6b0a7ace226a9737539155042d96e3a1f26b607
Guy Symonds <guy.symonds@honeywell.com>
2020-04-03 20:10:04 +0100

    added project to source

---
+++ src/code/RedSpartan.IntervalTraining/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RedSpartan.IntervalTraining.Models;
+
+namespace RedSpartan.IntervalTraining.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
