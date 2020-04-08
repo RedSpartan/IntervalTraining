c6757d32a1516bff82f3eb863dde677289abb2d5
Guy Symonds <guy@redspartan.co.uk>
2020-04-08 11:12:52 +0100

    Updated template

--- src/code/UI/Mobile/Shared/Models/IntervalTemplate.cs
+++ src/code/UI/Mobile/Shared/Models/IntervalTemplate.cs
@@ -17,6 +17,8 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.Models
         public string Name { get => _name; set => SetProperty(ref _name, value); }
         public int? TimeSeconds { get => _timeSeconds; set => SetProperty(ref _timeSeconds, value); }
         public int? Iterations { get => _iterations; set => SetProperty(ref _iterations, value); }
+
+        public int TotalIntervals => Intervals.Count;
         #endregion Properties
 
         #region Collections
@@ -24,5 +26,22 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.Models
 
         public ObservableCollection<History> History { get; set; } = new ObservableCollection<History>();
         #endregion Collections
+
+        public IntervalTemplate()
+        {
+            Intervals.CollectionChanged += Intervals_CollectionChanged;
+        }
+
+        private void Intervals_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Reset:
+                    RaisePropertyChanged(nameof(TotalIntervals));
+                    break;
+            }
+        }
     }
 }
