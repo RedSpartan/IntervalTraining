6cd81b15e9bc079070d44d9822ab82d98a4b41bf
Guy Symonds <guy@redspartan.co.uk>
2020-04-09 11:26:12 +0100

    Updated TimeViewModel

--- src/code/UI/Mobile/Shared/ViewModels/TimerViewModel.cs
+++ src/code/UI/Mobile/Shared/ViewModels/TimerViewModel.cs
@@ -4,16 +4,18 @@ using Prism.Navigation;
 using RedSpartan.IntervalTraining.Common;
 using RedSpartan.IntervalTraining.UI.Mobile.Shared.Models;
 using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.ViewModels
 {
-    public class TimerViewModel : ViewModelBase
+    public class TimerViewModel : ViewModelBase, IInitializeAsync
     {
         #region Fields
         private IntervalTemplate _intervalTemplate;
         private CountDownTimer _timer = new CountDownTimer();
         private string _timeRemaining = "00:00.000";
+        private bool _started = false;
         #endregion  Fields
 
         #region Properties
@@ -36,6 +38,10 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.ViewModels
         }
         #endregion Properties
 
+        #region Collections
+        public ObservableQueue<Interval> Queue { get; } = new ObservableQueue<Interval>();
+        #endregion Collections
+
         #region Services
         public IEventAggregator EventAggregator { get; }
         #endregion Services
@@ -43,6 +49,8 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.ViewModels
         #region Commands
         public ICommand StartCommand { get; }
         public ICommand StopCommand { get; }
+        public ICommand CloseCommand { get; }
+        
         #endregion Commands
 
         #region Constructor
@@ -51,13 +59,15 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.ViewModels
             : base(navigationService)
         {
             EventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
-            StartCommand = new DelegateCommand(() => Timer.Start());
-            StopCommand = new DelegateCommand(() => Timer.Pause());
+            StartCommand = new DelegateCommand(() => { _started = true; Timer.Start(); });
+            StopCommand = new DelegateCommand(() => { Timer.Pause(); _started = false; });
+            CloseCommand = new DelegateCommand(async () => await NavigationService.GoBackAsync());
         }
         #endregion Constructor
 
         #region Methods
-        public override void OnNavigatedTo(INavigationParameters parameters)
+
+        public async Task InitializeAsync(INavigationParameters parameters)
         {
             if (parameters.ContainsKey(nameof(IntervalTemplate)))
             {
@@ -65,13 +75,80 @@ namespace RedSpartan.IntervalTraining.UI.Mobile.Shared.ViewModels
             }
             base.OnNavigatedTo(parameters);
 
-            Timer.SetTime(0, 10);
+            CreateQueue(IntervalTemplate);
 
+            SetupTimer();
+            
             Timer.TimeChanged += () => TimeRemaining = Timer.TimeLeftMsStr;
 
-            Timer.CountDownFinished += () => TimeRemaining = "FINISHED";
+            Timer.CountDownFinished += () => OnCountDownFinish();
 
             Timer.StepMs = 33;
+        }
+
+        private void OnCountDownFinish()
+        {
+            Queue.Dequeue();
+
+            if (Queue.Count > 0)
+            {
+                SetupTimer();
+            }
+            else
+            {
+                _started = false;
+                TimeRemaining = "Finished";
+            }
+        }
+
+        private void SetupTimer()
+        {
+            Timer.SetTime(0, Queue.Peek().TimeSeconds);
+
+            TimeRemaining = Timer.TimeLeftMsStr;
+            
+            if (_started)
+            {
+                Timer.Start();
+            }
+        }
+
+        private void CreateQueue(IntervalTemplate template)
+        {
+            if (template.Iterations != null)
+            {
+                for (int i = 0; i < template.Iterations; i++)
+                {
+                    foreach (var item in template.Intervals)
+                    {
+                        Queue.Enqueue(item);
+                    }
+                }
+            }
+            else if(template.TimeSeconds != null)
+            {
+                int totalTime = 0;
+                while (totalTime < template.TimeSeconds)
+                {
+                    foreach (var item in template.Intervals)
+                    {
+                        totalTime += item.TimeSeconds;
+                        Queue.Enqueue(item);
+
+                        if(totalTime >= template.TimeSeconds)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in template.Intervals)
+                {
+                    Queue.Enqueue(item);
+                }
+            }
         }
 
         public override void Destroy()
